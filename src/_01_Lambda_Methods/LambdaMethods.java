46e14acc7227304d95d58dfe7eb88ed881dcda20
Meijia He <hemeijia0602@gmail.com>
2022-03-12 12:57:39 -0800

    finished lambda methods

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,13 +13,46 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			String reverse = "";
+			for(int i = s.length()-1; i >= 0; i--) {
+				reverse = reverse + s.charAt(i);
+			}
+			System.out.println(reverse);
+		}, "backwards");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String temp = "";
+			for(int i = 0; i <s.length(); i++) {
+				if (i%2==0) {
+			temp = temp + s.substring(i,i+1).toUpperCase();
+				}
+				else {
+			temp = temp + s.substring(i,i+1).toLowerCase();
+			//substring gives us one letter stuck into a string object started from i to i+1. 
+			// the end value is not included, only start is included
+				}
+			}
+			System.out.println(temp);
+		}, "hello");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String temp = "";
+			for(int i = 0; i <s.length(); i++) {
+				temp = temp + s.substring(i,i+1) + "." + s.substring(s.length());
+			}
+			System.out.println(temp);
+		}, "happy");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((s)->{
+			String vowels = "aeiouAEIOU";
+			for (int k=0;k<vowels.length();k++) {
+			s = s.replace(vowels.charAt(k)+"", "");
+					// now we can replace with string values
+			// we don't need if statements nor for loop of the string length bc every vowel value just replace it
+				}
+			System.out.println(s);
+		}, "watermelon");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
